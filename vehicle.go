9179d255bf8e602f197aa8f1f0ee87ccedb97cfc
Wasim Raaz Akram <wasimraazakram@gmail.com>
2022-07-16 15:17:34 +0530

    task 1

--- vehicle.go
+++ vehicle.go
@@ -4,10 +4,51 @@ import (
 
 )
 
+type vehicle interface{
+
+}
+
+type car struct{
+	mode1 string
+	make string
+	typeVehicle string
+}
+
+type truck struct{
+	mode1 string
+	make string
+	typeVehicle string
+}
+
+type bike struct{
+	mode1 string
+	make string
+}
+
+
 
 // Values array for the feedback.json file
+type Values struct{
+	Models []Mode1 'json:"values"'
+
+}
 
 // Model array for the feedback.json file
+type mode1 struct{
+	Name string 'json:"mode1"'
+	Feedback []string 'json:"feedback"'
+}
+
+type feedbackResult struct{
+	feedbackTotal int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral int
+}
+
+var vehicleResult map[string] feedbackResult
+
+var inventory []vehicle
 
 type rating float32
 
@@ -21,7 +62,7 @@ const (
 
 func init() {
 
-/*
+
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,7 +75,7 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
+
 //	vehicleResult = make(map[string]feedbackResult)
 
 }
